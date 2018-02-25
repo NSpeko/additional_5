fd50ae8b4fd93512218c5b5599af64eb75b173c6
NSpeko <nonstoper42@gmail.com>
2018-02-25 22:56:26 +0300

    Feature: Tested something, but not implemented :(

--- src/index.js
+++ src/index.js
@@ -1,3 +1,10 @@
 module.exports = function check(str, bracketsConfig) {
   // your solution
+    
+    console.log(str);
+    str=str.replace(/\(\)/,'').replace(/\(\)/,'').replace(/\(\)/,'').replace(/\(\)/,'');
+    if(str===''){
+        return true;
+    }
+    else return false;
 }
