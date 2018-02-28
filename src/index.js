4fdd34c65e71d156167c13ddea482ca613131e37
NSpeko <nonstoper42@gmail.com>
2018-02-28 10:33:13 +0300

    Fix: Finaly inplemented

--- src/index.js
+++ src/index.js
@@ -1,8 +1,14 @@
 module.exports = function check(str, bracketsConfig) {
   // your solution
+    let conf='([{|';
     let configArr=[];
     for(let i=0;i<bracketsConfig.length;i++){
-        configArr.push(new RegExp('\\'+bracketsConfig[i][0]+'\\'+bracketsConfig[i][1]));//bracketsConfig[i][0]+bracketsConfig[i][1]);
+        if(conf.includes(bracketsConfig[i][0])){
+            configArr.push(new RegExp('\\'+bracketsConfig[i][0]+'\\'+bracketsConfig[i][1]));//bracketsConfig[i][0]+bracketsConfig[i][1]);
+        }
+        else{
+            configArr.push(new RegExp(bracketsConfig[i][0]+bracketsConfig[i][1]));
+        }
     }
     while(str!==''){
         let tempStrChecker=str;
